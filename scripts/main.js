0aac922e85f20c153854218d75d8838d5099ffe4
Noorarjun Sidhu <nsinghsidhu12@gmail.com>
2021-11-27 21:01:53 -0800

    added modal for notifications, will link to other html pages.

--- scripts/main.js
+++ scripts/main.js
@@ -15,29 +15,11 @@ function insertName() {
           //document.getElementById("name-goes-here").innerText = n;    //using javascript
           //method #2:  insert using jquery
           $("#name-goes-here").text(user_Name); //using jquery
-
-          //Notifcation button click
-          document.getElementById("notification").addEventListener("click", function (e) {
-            e.preventDefault();
-
-            let read = db.collection("users").doc(user.uid);
-
-            read.get()
-              .then(doc => {
-                if (doc.data().noti != null) {
-                  // doc.data() is never undefined for query doc snapshots
-                  alert(doc.data().recentSender + " just sent you a message! Message back using this chat ID! " + doc.data().noti);
-                } else {
-                  alert("No messages yet");
-                }
-              })
-
-
-          }, false);
         })
     } else {
       // No user is signed in.
     }
   });
 }
+
 insertName();

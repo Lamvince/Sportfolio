7cb5ab13bad427596f3227a326ed34e1176b0f14
ryanrudzitis <ryanrudzitis@icloud.com>
2021-12-03 14:05:23 -0800

    edited some comments

--- scripts/users.js
+++ scripts/users.js
@@ -19,7 +19,6 @@ function showAllUsers() {
               newCard.getElementById("userspfp").src = doc.data().userpfp;
               newCard.querySelector('.card-href').href = "chat.html?uid=" +
                 doc.id + "&name=" + doc.data().name; //pass name and id
-              // Append newCard to friends-go-here div
               document.getElementById("friends-go-here").appendChild(newCard);
 
             }
